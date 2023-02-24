705a82b596659659d0af00914cddeadc11e909cc
kamleshmore <kamlesh@plan2intl.com>
2023-02-24 18:47:55 +0530

    Refreshing the hole code

--- src/app/allMainComponents/content-pages/sub-pages.modle.ts
+++
@@ -1,3 +0,0 @@
-export class subPages {
-  constructor(public heading: string, public description: string) {}
-}
--- src/app/core/models/user.ts
+++
@@ -1,9 +0,0 @@
-export class User {
-  id: number;
-  img: string;
-  username: string;
-  password: string;
-  firstName: string;
-  lastName: string;
-  token: string;
-}
